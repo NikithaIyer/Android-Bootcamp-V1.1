28f2349dc371c9dd33995d9163d2a99b33a800cc
Nikitha <nikita9.iyer@gmail.com>
2014-02-20 13:26:04 +0530

    Opening a new activity. to do set the title

---
+++ src/bootcamp/android/activities/ProductDescription.java
@@ -0,0 +1,26 @@
+package bootcamp.android.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+import bootcamp.android.R;
+
+/**
+ * Created by nikitha on 20/02/14.
+ */
+public class ProductDescription extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.product);
+        Intent intent = getIntent();
+//        String resultValue = intent.getStringExtra("result");
+//
+//        TextView resultView = (TextView) findViewById(R.id.title);
+//        resultView.setText((CharSequence) resultView);
+
+    }
+
+
+
+}
